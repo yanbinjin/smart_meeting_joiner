1b8d237073be584e6ca15689f1e8c873175e803d
yanbinjin <yanbin.jin@dailypay.com>
2024-03-13 22:53:57 -0700

    config example

---
+++ config.js
@@ -0,0 +1,5 @@
+export default {
+  clientId: 'YOUR_CLIENT_ID.apps.googleusercontent.com',
+  clientSecret: 'client_secret from the downlaod',
+  redirectUri: 'http://localhost:3000/oauth2callback',
+};
